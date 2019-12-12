776d76049e20d683a94dfa4ddbbfa01c4e55aa8f
servismas <servismas@gmail.com>
2019-12-12 19:00:18 +0200

    Added DB and seed

--- news/resources/views/welcome.blade.php
+++ news/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello, Dima
                 </div>
 
                 <div class="links">
